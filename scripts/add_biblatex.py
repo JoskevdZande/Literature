7f420ed21a919a40b6bba0a619e6babe39850b30
drepeeters <dre.peeters@hotmail.com>
2023-09-06 16:51:23 +0200

    correct diab.bib path

--- scripts/add_biblatex.py
+++ scripts/add_biblatex.py
@@ -1,6 +1,6 @@
 from scripts.get_biblatex import GetBiblatex
 
-diag_bib = r"C:\Users\drepeeters\OneDrive - Radboudumc\Desktop\webteam\Literature\diag.bib"
+diag_bib = r"..\diag.bib"
 doi = "10.1186/s12903-023-03362-8"
 reader = GetBiblatex(diag_bib=diag_bib, doi=doi)
 
