4b3a2104aa89ce3628e70f0294341a80e9d3e4ca
drepeeters <dre.peeters@hotmail.com>
2023-09-20 14:20:47 +0200

    Fix issue with bibkeys

--- scripts/add_biblatex.py
+++ scripts/add_biblatex.py
@@ -1,8 +1,28 @@
 from scripts.get_biblatex import GetBiblatex
+import os
 
-diag_bib = r"..\diag.bib"
-doi = "10.1186/s12903-023-03362-8"
-reader = GetBiblatex(diag_bib=diag_bib, doi=doi)
+
+def get_citations(ss_id):
+    from semanticscholar import SemanticScholar
+    sch = SemanticScholar()
+    paper = sch.get_paper(ss_id)
+
+    return len(paper['citations'])
+
+
+cwd = os.getcwd()
+diag_bib_path = os.path.join(cwd, 'diag.bib')
+
+with open(diag_bib_path, encoding="utf8") as bibtex_file:
+    diag_bib = bibtex_file.read()
+
+
+doi = "10.1016/j.modpat.2023.100233"
+ss_id = ""
+
+# citations = get_citations(ss_id)
+citations = 0
+reader = GetBiblatex(diag_bib=diag_bib, doi=doi, num_citations=citations)
 
 response = reader.get_bib_text()
 print(response)
